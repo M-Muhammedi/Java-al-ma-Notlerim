281ebd7e12d4e546e310dc346c5d9f9a7eadeb7d
Mustafa Muhammedi <146854084+M-Muhammedi@users.noreply.github.com>
2024-02-20 15:03:51 +0300

    Add files via upload

---
+++ 01ders/src/Merkez.java
@@ -0,0 +1,13 @@
+
+public class Merkez {
+
+	public static void main(String[] args) {
+		 int vize1 = 76 ;
+		 int vize2 = 60 ;
+		 int finalNotu = 40 ;
+		 
+		 int sonuc = (vize1+vize2+finalNotu)/3;
+		 System.out.println("Ders Geçme Notunuz :"+sonuc);
+	}
+
+}
