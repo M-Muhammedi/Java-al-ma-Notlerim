ef03df5a2f0d869da96c6badc752484e381162e1
Mustafa Muhammedi <146854084+M-Muhammedi@users.noreply.github.com>
2024-02-24 12:00:29 +0300

    Add files via upload

--- exemple.java
+++ exemple.java
@@ -12,6 +12,9 @@ public class exemple {
 		System.out.print("Soyadınızı Giriniz : ");
 		String soyisim = scanner.nextLine();
 		
+		System.out.print("Dersinizi Giriniz : ");
+		String ders = scanner.nextLine();
+		
 		System.out.print("Vize Notunuzu Giriniz : ");
 		int vize1 = scanner.nextInt();
 		
@@ -20,9 +23,18 @@ public class exemple {
 		
 		double ortalama = (vize1*0.4)+(final1*0.6);
 		
+		System.out.println("----------------- Ortalama Bulma Uygulaması -----------------");
+		
 		System.out.println("Adınız : "+ isim);
 		System.out.println("Soyadınız : "+ soyisim);
-		System.out.print("Sınav Ortalamanız :"+ ortalama);
+		System.out.println("Dersiniz : "+ ders);
+		System.out.println("Sınav Ortalamanız : "+ ortalama);
+		
+		if(ortalama >= 50) {
+			System.out.println("Tebrikler Geçtiniz !!! ");
+		}else {
+			System.out.println("Üzgünüm Kaldınız :( ");
+		}
 
 	}
 
